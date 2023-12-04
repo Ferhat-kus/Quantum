bd854acc53c731f001eb62165828fd748d2bf644
Ferhat-kus <55ferhat.kus@gmail.com>
2023-12-04 18:52:52 +0300

    review yapıldı

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
-import { Text, StyleSheet, View, StatusBar } from 'react-native'
+import {StyleSheet, View, StatusBar } from 'react-native'
 import React, { Component } from 'react'
-import SelfPage from './src/pages/SelfPage/Index';
-import SelfConcordance from './src/pages/SelfConcordance/Index';
-import CompatibilityAnalysis from './src/pages/CompatibilityAnalysis/Index'
-import PaymentPlan from './src/pages/PaymentPlan/Index'
 import Navigation from './src/router/Index'
 
 export default class App extends Component {
